74c06b73724cb5f3e1036bc4a44c235baeaa8ede
EC2 Default User <ec2-user@ip-172-31-25-143.ap-northeast-1.compute.internal>
2023-03-02 17:31:10 +0000

    応用課題5

---
+++ exercise5/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#top"
+  get 'home/about', to: 'home#about'
+  get "search" => "searches#search"
+
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:show, :edit, :update, :index]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+end
