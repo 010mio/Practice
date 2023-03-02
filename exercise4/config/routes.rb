44fee61153f7b71c61414bd66dbe26c4bbb45247
EC2 Default User <ec2-user@ip-172-31-25-143.ap-northeast-1.compute.internal>
2023-03-02 14:30:45 +0000

    応用課題4

--- exercise4/config/routes.rb
+++ exercise4/config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:show, :edit, :update, :index]do
-    resources :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
 
 end
